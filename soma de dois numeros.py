75ebc9ab63f3556a1c8ecc8734da10cb0638e619
nickolassoares007 <nickass.soares007@outlook.com>
2025-04-13 15:36:05 -0300

    Tirei o # do final

--- soma de dois numeros.py
+++ soma de dois numeros.py
@@ -5,4 +5,4 @@ a = int(input("Digite o primeiro número: ")) #Aqui estou pedindo o primeiro nú
 b = int(input("Digite o segundo número: ")) #Aqui estou pedindo o segundo número
 
 result = addition(a, b) #Aqui estou chamando a função de soma e passando os números como parâmetros
-print(f"A soma de {a} e {b} é: {result}") #
+print(f"A soma de {a} e {b} é: {result}")
